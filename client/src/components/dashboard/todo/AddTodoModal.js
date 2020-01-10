92154277c5477992469f17d652143f1e207d69cb
Abdul-fatai <staticdev20046@gmail.com>
2020-01-10 12:29:25 +0100

    clean up

--- client/src/components/dashboard/todo/AddTodoModal.js
+++ client/src/components/dashboard/todo/AddTodoModal.js
@@ -5,7 +5,32 @@ const AddTodoModal = () => {
   return (
     <div id="add-todo-modal" className="modal">
       <div className="modal-content">
-        <h4 className="center">New Technician</h4>
+        <h4 className="center">Add a Todo</h4>
+        <form>
+          <div className="card-content">
+            <div className="row">
+              <div className="input-field col s12">
+                <label htmlFor="email">Title</label>
+                <input type="text" className="validate" name="name" />
+              </div>
+              <div className="input-field col s12">
+                <label htmlFor="email">Description</label>
+                <textarea
+                  name="desc"
+                  className="materialize-textarea"
+                ></textarea>
+              </div>
+            </div>
+            <div className="card-action right-align center text-white">
+              <button
+                className="btn blue waves-effect waves-light"
+                type="submit"
+              >
+                Submit <i className="material-icons right">send</i>
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
--- client/src/components/dashboard/todo/EditTodoModal.js
+++ client/src/components/dashboard/todo/EditTodoModal.js
@@ -4,7 +4,40 @@ const EditTodoModal = () => {
   return (
     <div id="edit-todo-modal" className="modal">
       <div className="modal-content">
-        <h4 className="center">New Technician</h4>
+        <h4 className="center">Edit a Todo</h4>
+        <form>
+          <div className="card-content">
+            <div className="row">
+              <div className="input-field col s12">
+                <label htmlFor="title">Title</label>
+                <input type="text" className="validate" name="name" />
+              </div>
+              <div className="input-field col s12">
+                <label htmlFor="desc">Description</label>
+                <textarea
+                  name="desc"
+                  className="materialize-textarea"
+                ></textarea>
+              </div>
+              <div className="input-field col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" name="" className="filled-in" />
+                    <span>Completed</span>
+                  </label>
+                </p>
+              </div>
+            </div>
+            <div className="card-action right-align center text-white">
+              <button
+                className="btn blue waves-effect waves-light"
+                type="submit"
+              >
+                Updated <i className="material-icons right">send</i>
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
