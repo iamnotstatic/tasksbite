ed5d0d4f7aabf7f4b041e19eab57790b128a4047
Abdul-fatai <staticdev20046@gmail.com>
2020-01-13 12:09:14 +0100

    added auth check  on 404 page

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import Spinner from '../layout/Spinner';
+import AuthContext from '../../context/auth/authContext';
 
 const NotFound = () => {
+  const authContext = useContext(AuthContext);
+  const { loading, isAuthenticated, loadUser } = authContext;
+
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <div className="valign-wrapper row">
       <div className="col card s10 pull-s1 m6 pull-m3 l4 pull-l4">
@@ -17,9 +31,15 @@ const NotFound = () => {
           >
             Page not Found
           </p>
-          <a href="/" className="btn blue">
-            Back to home
-          </a>
+          {isAuthenticated ? (
+            <a href="/dashboard" className="btn blue">
+              Back to Dashboard
+            </a>
+          ) : (
+            <a href="/" className="btn blue">
+              Back to home
+            </a>
+          )}
         </div>
       </div>
     </div>
