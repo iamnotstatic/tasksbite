9b3c51a1f21e39126c77ef0b158b1c32dbcd643d
Abdul-fatai <staticdev20046@gmail.com>
2020-01-18 02:44:56 +0100

    deplyment proccess

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ const app = require('./app');
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
 
-  // Join file
-  const publicDir = path.join(__dirname, '../client/build/index.html');
+  const publicDir = path.join(__dirname, './client/build/index.html');
   app.use(express.static(publicDir));
 
   app.get('*', (req, res) => {
-    req.sendFile(
+    res.sendFile(
       path.resolve(__dirname, 'src', 'client', 'build', 'index.html')
     );
   });
