35caa4cf9fd23a4c15a666bbbfbaf7bd5c4dfd04
Abdul-fatai <staticdev20046@gmail.com>
2020-01-18 01:49:43 +0100

    Prepare for deploy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+const path = require('path');
 const app = require('./app');
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    req.sendFile(
+      path.resolve(__dirname, 'src', 'client', 'build', 'index.html')
+    );
+  });
+}
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
